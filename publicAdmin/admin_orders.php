d161595cbf7cb157ea5db4b0c291439a37a110f2
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-11-03 22:24:24 +0800

    updated UI

--- publicAdmin/admin_orders.php
+++ publicAdmin/admin_orders.php
@@ -55,15 +55,15 @@ require_once "$path/private/head.php";
             while($fetch_orders = $select_orders->fetch(PDO::FETCH_ASSOC)){
       ?>
       <div class="box">
-         <p> user id : <span><?= $fetch_orders['user_id']; ?></span> </p>
-         <p> placed on : <span><?= $fetch_orders['placed_on']; ?></span> </p>
-         <p> name : <span><?= $fetch_orders['name']; ?></span> </p>
-         <p> email : <span><?= $fetch_orders['email']; ?></span> </p>
-         <p> number : <span><?= $fetch_orders['number']; ?></span> </p>
-         <p> address : <span><?= $fetch_orders['address']; ?></span> </p>
-         <p> total products : <span><?= $fetch_orders['total_products']; ?></span> </p>
-         <p> total price : <span>$<?= $fetch_orders['total_price']; ?>/-</span> </p>
-         <p> payment method : <span><?= $fetch_orders['method']; ?></span> </p>
+         <p> user id: <span><?= $fetch_orders['user_id']; ?></span> </p>
+         <p> placed on: <span><?= $fetch_orders['placed_on']; ?></span> </p>
+         <p> name: <span><?= $fetch_orders['name']; ?></span> </p>
+         <p> email: <span><?= $fetch_orders['email']; ?></span> </p>
+         <p> number: <span><?= $fetch_orders['number']; ?></span> </p>
+         <p> address: <span><?= $fetch_orders['address']; ?></span> </p>
+         <p> total products: <span><?= $fetch_orders['total_products']; ?></span> </p>
+         <p> total price: <span>$<?= $fetch_orders['total_price']; ?></span> </p>
+         <p> payment method: <span><?= $fetch_orders['method']; ?></span> </p>
          <form action="" method="POST">
             <input type="hidden" name="order_id" value="<?= $fetch_orders['id']; ?>">
             <select name="update_payment" class="drop-down">
@@ -72,7 +72,7 @@ require_once "$path/private/head.php";
                <option value="completed">completed</option>
             </select>
             <div class="flex-btn">
-               <input type="submit" name="update_order" class="option-btn" value="udate">
+               <span class="option-btn"><input type="submit" name="update_order" class="submit" value="udate"></span>
                <a href="admin_orders.php?delete=<?= $fetch_orders['id']; ?>" class="delete-btn" onclick="return confirm('delete this order?');">delete</a>
             </div>
          </form>
