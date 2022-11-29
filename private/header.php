a0659580202fe0f9df73737705236a6c55100290
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-11-29 21:26:21 +0800

    added modal shopping cart menu

--- private/header.php
+++ private/header.php
@@ -15,6 +15,28 @@ if(isset($message)){
    }
 }
 
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE id = ?");
+   $delete_cart_item->execute([$delete_id]);
+   // header('location:cart');
+}
+
+if(isset($_GET['delete_all'])){
+   $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+   $delete_cart_item->execute([$user_id]);
+   // header('location:cart');
+}
+
+if(isset($_POST['update_qty'])){
+   $cart_id = $_POST['cart_id'];
+   $p_qty = clearValue($_POST['p_qty']);
+   $update_qty = $conn->prepare("UPDATE `cart` SET quantity = ? WHERE id = ?");
+   $update_qty->execute([$p_qty, $cart_id]);
+   $message[] = 'cart quantity updated';
+}
+
+
 
 ?>
 
@@ -58,17 +80,96 @@ if(isset($message)){
          </svg></a>
          <!-- <a href="search" class="fas fa-search search-icon"></a> -->
          <div id="mySideCart" class="side-cart">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <svg class="theme-icon" id="close-btn" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <!-- <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a> -->
+            <svg class="theme-icon closebtn" onclick="closeNav()" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                <path d="M46 2L2 46" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                <path d="M46 46L2 2" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
             </svg>
-            <a href="#">About</a>
+            <!-- <a href="#">About</a>
             <a href="#">Services</a>
             <a href="#">Clients</a>
-            <a href="#">Contact</a>
+            <a href="#">Contact</a> -->
+            <section class="shopping-cart">
+
+               <h1 class="title">your cart</h1>
+
+               <div class="box-container">
+
+               <?php
+                  $grand_total = 0;
+                  $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+                  $select_cart->execute([$user_id]);
+                  if($select_cart->rowCount() > 0){
+                     while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){ 
+               ?>
+               <form action="" method="POST" class="box">
+                  <a href="cart?delete=<?= $fetch_cart['id'];?>" onclick="return confirm('delete this from cart?');"> 
+                     <svg class="theme-icon" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M46 2L2 46" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                     <path d="M46 46L2 2" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                     </svg>
+                  </a>
+                  <!-- <a href="cart?delete=<?= $fetch_cart['id']; ?>" class="fas fa-times" onclick="return confirm('delete this from cart?');"></a> -->
+                  <img src="uploaded_img/<?= $fetch_cart['image']; ?>" alt="">
+                  <div class="name"><?= $fetch_cart['name']; ?></div>
+                  <div class="price">$<?= $fetch_cart['price']; ?></div>
+                  <input type="hidden" name="cart_id" value="<?= $fetch_cart['id']; ?>">
+                  <div class="flex-btn">
+                     <input type="number" min="1" value="<?= $fetch_cart['quantity']; ?>" class="qty" name="p_qty">
+                     <span class="option-btn"><input type="submit" value="update" name="update_qty" class="submit"></span>
+                  </div>
+                  <div class="sub-total"> sub total: <span>$<?= $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?></span> </div>
+               </form>
+               <?php
+                  $grand_total += $sub_total;
+                  }
+               }else{
+                  echo '<p class="empty">your cart is empty</p>';
+               }
+               ?>
+               </div>
+
+               <!-- <div class="box-container">
+
+               <?php
+                  $grand_total = 0;
+                  $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+                  $select_cart->execute([$user_id]);
+                  if($select_cart->rowCount() > 0){
+                     while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){ 
+               ?>
+               <form action="" method="POST" class="box">
+                  <a href="cart?delete=<?= $fetch_cart['id']; ?>" class="fas fa-times" onclick="return confirm('delete this from cart?');"></a>
+                  <a href="view?pid=<?= $fetch_cart['pid']; ?>" class="fas fa-eye"></a>
+                  <img src="uploaded_img/<?= $fetch_cart['image']; ?>" alt="">
+                  <div class="name"><?= $fetch_cart['name']; ?></div>
+                  <div class="price">$<?= $fetch_cart['price']; ?></div>
+                  <input type="hidden" name="cart_id" value="<?= $fetch_cart['id']; ?>">
+                  <div class="flex-btn">
+                     <input type="number" min="1" value="<?= $fetch_cart['quantity']; ?>" class="qty" name="p_qty">
+                     <span class="option-btn"><input type="submit" value="update" name="update_qty" class="submit"></span>
+                  </div>
+                  <div class="sub-total"> sub total : <span>$<?= $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?></span> </div>
+               </form>
+               <?php
+                  $grand_total += $sub_total;
+                  }
+               }else{
+                  echo '<p class="empty">your cart is empty</p>';
+               }
+               ?>
+               </div> -->
+
+               <div class="cart-total">
+                  <p>grand total: <span>$<?= $grand_total; ?></span></p>
+                  <!-- <a href="shop" class="option-btn">continue shopping</a> -->
+                  <a href="cart?delete_all" class="delete-btn <?= ($grand_total > 1)?'':'disabled'; ?>">delete all</a>
+                  <a href="checkout" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>">proceed to checkout</a>
+               </div>
+
+            </section>
          </div>
-         <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+         <!-- <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span> -->
          <?php
             $count_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
             $count_cart_items->execute([$user_id]);
@@ -76,22 +177,12 @@ if(isset($message)){
             $count_wishlist_items->execute([$user_id]);
          ?>
          <!-- <a href="wishlist"><i class="fas fa-heart"></i><span>(<?= $count_wishlist_items->rowCount(); ?>)</span></a> -->
-         <div href="cart" class="cart" ><svg class="theme-icon" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <div href="cart" class="cart" onclick="openNav()" ><svg class="theme-icon" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M7.84052 11.5652H43.4958C43.79 11.5652 44.0802 11.6329 44.3439 11.7633C44.6076 11.8936 44.8377 12.0831 45.0163 12.3168C45.1949 12.5506 45.3172 12.8223 45.3737 13.111C45.4301 13.3997 45.4193 13.6976 45.3419 13.9814L41.4144 28.379C41.0833 29.5936 40.362 30.6657 39.3615 31.4301C38.3611 32.1944 37.1371 32.6085 35.8781 32.6087H12.2711" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
             <path d="M2 2H2.72313C3.60097 2.00005 4.45209 2.30195 5.13375 2.85508C5.8154 3.4082 6.28612 4.1789 6.46696 5.03791L12.8393 35.3099C13.0201 36.1689 13.4909 36.9396 14.1725 37.4927C14.8542 38.0459 15.7053 38.3478 16.5831 38.3478H38.3478" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
             <path d="M12.5217 43.1304C12.5217 43.8915 12.8241 44.6214 13.3622 45.1595C13.9004 45.6977 14.6302 46 15.3913 46C16.1523 46 16.8822 45.6977 17.4204 45.1595C17.9585 44.6214 18.2609 43.8915 18.2609 43.1304C18.2609 42.3694 17.9585 41.6395 17.4204 41.1013C16.8822 40.5632 16.1523 40.2609 15.3913 40.2609C14.6302 40.2609 13.9004 40.5632 13.3622 41.1013C12.8241 41.6395 12.5217 42.3694 12.5217 43.1304V43.1304Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
             <path d="M31.6522 43.1304C31.6522 43.8915 31.9545 44.6214 32.4926 45.1595C33.0308 45.6977 33.7607 46 34.5217 46C35.2828 46 36.0127 45.6977 36.5508 45.1595C37.089 44.6214 37.3913 43.8915 37.3913 43.1304C37.3913 42.3694 37.089 41.6395 36.5508 41.1013C36.0127 40.5632 35.2828 40.2609 34.5217 40.2609C33.7607 40.2609 33.0308 40.5632 32.4926 41.1013C31.9545 41.6395 31.6522 42.3694 31.6522 43.1304V43.1304Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-         </svg><span><?= $count_cart_items->rowCount(); ?></span>
-         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <svg class="theme-icon" id="close-btn" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-               <path d="M46 2L2 46" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-               <path d="M46 46L2 2" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
-         </div>
+            </svg><span><?= $count_cart_items->rowCount(); ?></span></div>
          <!-- <a href="cart"><i class="fas fa-shopping-cart"></i><span>(<?= $count_cart_items->rowCount(); ?>)</span></a> -->
       </div>
 
@@ -121,10 +212,34 @@ if(isset($message)){
 
 <script>
 function openNav() {
-  document.getElementById("mySideCart").style.width = "250px";
+   function mediaQuery(width) {
+      if (width.matches) {
+         document.getElementById("mySideCart").style.width = "100%";
+      } else {
+         document.getElementById("mySideCart").style.width = "62%";
+      
+      }
+//   document.getElementById("cartMenu").style.width = "250px";
+   }
+   let width = window.matchMedia("(max-width: 806px)");
+   mediaQuery(width);
+   width.addListiner(mediaQuery);
 }
 
 function closeNav() {
   document.getElementById("mySideCart").style.width = "0";
+//   document.getElementById("cartMenu").style.width = "0";
 }
+
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//     document.body.style.backgroundColor = "yellow";
+//   } else {
+//    document.body.style.backgroundColor = "pink";
+//   }
+// }
+
+// var x = window.matchMedia("(max-width: 700px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
 </script>
